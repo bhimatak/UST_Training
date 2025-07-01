d22f897a95e1ecd5bc0b7e77c91251316b8d2b26
bhimatak <bhima.t@gmail.com>
2025-07-01 14:20:57 +0530

    updated

--- out/build/x64-debug/CTestTestfile.cmake
+++ out/build/x64-debug/CTestTestfile.cmake
@@ -1,8 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: C:/Users/Administrator/Desktop/PrjTest/MyProject
-# Build directory: C:/Users/Administrator/Desktop/PrjTest/MyProject/out/build/x64-debug
+# Source directory: C:/Users/Administrator/Desktop/MyProject
+# Build directory: C:/Users/Administrator/Desktop/MyProject/out/build/x64-debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(RunMyTests "C:/Users/Administrator/Desktop/PrjTest/MyProject/out/build/x64-debug/MyTests.exe")
-set_tests_properties(RunMyTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Administrator/Desktop/PrjTest/MyProject/CMakeLists.txt;27;add_test;C:/Users/Administrator/Desktop/PrjTest/MyProject/CMakeLists.txt;0;")
+add_test(RunMyTests "C:/Users/Administrator/Desktop/MyProject/out/build/x64-debug/MyTests.exe")
+set_tests_properties(RunMyTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Administrator/Desktop/MyProject/CMakeLists.txt;27;add_test;C:/Users/Administrator/Desktop/MyProject/CMakeLists.txt;0;")
